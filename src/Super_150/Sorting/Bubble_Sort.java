81c255b2197137bccefb8c1d1252a02bb395fac0
Rohit <rohit.gla_cs21@gla.ac.in>
2022-11-15 07:23:44 +0530

    This is my bubble sort code

--- src/Super_150/Sorting/Bubble_Sort.java
+++ src/Super_150/Sorting/Bubble_Sort.java
@@ -1,8 +1,15 @@
-package CLASSES_150.Sorting;
+package Super_150.Sorting;
+
+import java.util.Scanner;
 
 public class Bubble_Sort {
     public static void main(String[] args) {
-        int[] arr = {3, 5, 4, 2, 1};
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int[]arr=new int[n];
+        for(int i=0; i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
         Sort(arr);
         for (int i = 0; i < arr.length ; i++) {
             System.out.println(arr[i]+ " ");
@@ -12,13 +19,17 @@ public class Bubble_Sort {
 
     public static void Sort(int[] arr) {
         for (int j = 0; j < arr.length; j++) {
-            for (int i = 0; i < arr.length ; i++) {
-                if (arr[i] > arr[i]) {
+            int swap=0;
+            for (int i = 0; i < arr.length-1-j ; i++) {
+                if (arr[i] > arr[i+1]) {
+                    swap++;
                     int temp = arr[i];
                     arr[i] = arr[i + 1];
                     arr[i + 1] = temp;
                 }
             }
+            System.out.print("SWAP IS "+swap+" ");
         }
+
     }
 }
