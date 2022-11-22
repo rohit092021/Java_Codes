20dcfcdf856bbf0618fcba12280d6dc6d0e63867
Rohit <rohit.gla_cs21@gla.ac.in>
2022-11-22 14:54:13 +0530

    Array list demo

---
+++ src/Super_150/Array_List/Array_list_demo.java
@@ -0,0 +1,22 @@
+package Super_150.Array_List;
+
+public class Array_list_demo {
+    public static void main(String[] args) {
+        Integer a=100;
+        int a1=10;
+        System.out.println(a);
+        System.out.println(a1);
+         Short s1=89;
+         Byte b=48;
+        System.out.println(s1);
+        System.out.println(b);
+
+        a=a1;// when primitive is put into its wrapper then it is known as Autoboxing
+        System.out.println(a);
+        Integer ii=100;
+        int i=10;
+        i=ii;//when wrapper is put into its primitive then it is known as Onboxing
+
+        System.out.println(i);
+    }
+}
