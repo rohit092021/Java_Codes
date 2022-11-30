f67356a42db5587fc3ae50cad2efe1c31cd9741c
Rohit <rohit.gla_cs21@gla.ac.in>
2022-11-30 09:15:11 +0530

    AVG of Sliding Window

---
+++ src/Super_150/Sliding_window/Average_of_sliding_window.java
@@ -0,0 +1,36 @@
+package Super_150.Sliding_window;
+import java.util.*;
+public class Average_of_sliding_window {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        ArrayList<Integer> list=new ArrayList<>();
+        int window_size=sc.nextInt();
+
+        while(true)
+        {
+            int ele=sc.nextInt();
+            if(ele==-1)
+                break;
+            list.add(ele);
+        }
+
+        double sum=0;
+        int j=0;     // to shrink the element from starting position.
+        for(int i=0;i<list.size();i++)
+        {
+            if(i<window_size)
+            {
+                sum+=list.get(i);
+                System.out.printf("%.4f ",sum/(i+1));
+            }
+            else
+            {
+                sum += list.get(i)-list.get(j);
+                System.out.printf("%.4f ",sum/(window_size));
+                j++;
+            }
+ }
+
+    }
+}
