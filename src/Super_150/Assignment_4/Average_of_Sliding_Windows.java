cbcd274f09fe0b4c8d2a8f040cccdbefc03c5668
Rohit <rohit.gla_cs21@gla.ac.in>
2022-12-12 12:38:50 +0530

    150 Assignment

---
+++ src/Super_150/Assignment_4/Average_of_Sliding_Windows.java
@@ -0,0 +1,30 @@
+package Super_150.Assignment_4;
+import java.util.*;
+public class Average_of_Sliding_Windows {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        int window_size = sc.nextInt();
+
+        while (true) {
+            int ele = sc.nextInt();
+            if (ele == -1)
+                break;
+            list.add(ele);
+        }
+
+        double sum = 0;
+        int j = 0;     // to shrink the element from starting position.
+        for (int i = 0; i < list.size(); i++) {
+            if (i < window_size) {
+                sum += list.get(i);
+                System.out.printf("%.4f ", sum / (i + 1));
+            } else {
+                sum += list.get(i) - list.get(j);
+                System.out.printf("%.4f ", sum / (window_size));
+                j++;
+            }
+        }
+    }
+}
