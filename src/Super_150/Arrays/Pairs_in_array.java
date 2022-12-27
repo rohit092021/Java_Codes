32f9e7c6fa2a3294cddd5074aa76230d7be9dd1a
Rohit <rohit.gla_cs21@gla.ac.in>
2022-12-27 09:25:21 +0530

    150 Assignment

--- src/Super_150/Arrays/Pairs_in_array.java
+++ src/Super_150/Arrays/Pairs_in_array.java
@@ -13,12 +13,16 @@ public class Pairs_in_array {
         printPairs(arr);
     }
     public static void printPairs(int[] arr){
+        int tp=0;
         for(int i=0;i<arr.length;i++){
             int curr=arr[i];
             for(int j=i+1;j<arr.length;j++){
                 System.out.print("("+curr+","+arr[j]+")");
+                tp++;
             }
             System.out.println();
         }
+        System.out.println("total pairs = "+tp);
+
     }
 }
