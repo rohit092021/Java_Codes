0b59c0b5ff3a497ce05187715d75dfdcf7c50d6c
Rohit <rohit.gla_cs21@gla.ac.in>
2022-12-31 06:49:57 +0530

    150 Assignment

--- src/Super_150/String/String_Builder.java
+++ src/Super_150/String/String_Builder.java
@@ -1,27 +1,27 @@
-package Super_150.String;
-
-public class String_Builder {
-    public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder("Hello");
-        System.out.println(sb.length());
-        sb.append('c');
-        System.out.println(sb.capacity());
-        StringBuilder sb1 = new StringBuilder();
-        sb.append("Bye");
-        System.out.println(sb);
-        System.out.println(sb.length());
-        sb.append("HellocBhelo1");
-        System.out.println(sb.length());
-        System.out.println(sb.capacity());
-        sb.append('a');
-        System.out.println(sb.capacity());
-        System.out.println(sb );
-        sb.insert(1, "ll");
-        System.out.println(sb);
-        String s = sb.toString();// Builder to String
-        System.out.println(sb.reverse());
-        System.out.println(sb.charAt(2));
-        System.out.println(sb.substring(1,3));
-
-    }
-}
+//package Super_150.String;
+//
+//public class String_Builder {
+//    public static void main(String[] args) {
+//        StringBuilders sb = new StringBuilders("Hello");
+//        System.out.println(sb.length());
+//        sb.append('c');
+//        System.out.println(sb.capacity());
+//        StringBuilders sb1 = new StringBuilders();
+//        sb.append("Bye");
+//        System.out.println(sb);
+//        System.out.println(sb.length());
+//        sb.append("HellocBhelo1");
+//        System.out.println(sb.length());
+//        System.out.println(sb.capacity());
+//        sb.append('a');
+//        System.out.println(sb.capacity());
+//        System.out.println(sb );
+//        sb.insert(1, "ll");
+//        System.out.println(sb);
+//        String s = sb.toString();// Builder to String
+//        System.out.println(sb.reverse());
+//        System.out.println(sb.charAt(2));
+//        System.out.println(sb.substring(1,3));
+//
+//    }
+//}
