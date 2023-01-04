fa3b26e8258256774e524b9474b68a2213d60e63
Rohit <rohit.gla_cs21@gla.ac.in>
2023-01-04 12:13:14 +0530

    java code

---
+++ src/Super_150/Recursion/print_n_to_1.java
@@ -0,0 +1,19 @@
+package Super_150.Recursion;
+
+import java.util.Scanner;
+
+public class print_n_to_1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        printNum(n);
+    }
+    public static void printNum(int n){
+        if(n==1){
+            System.out.println(1);
+            return;
+        }
+        System.out.println(n);
+        printNum(n-1);
+    }
+}
