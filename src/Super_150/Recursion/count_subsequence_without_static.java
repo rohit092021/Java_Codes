6c88db6c2168476c712f33772b43a7f2f5b580d2
Rohit <rohit.gla_cs21@gla.ac.in>
2023-01-12 10:54:51 +0530

    java code

---
+++ src/Super_150/Recursion/count_subsequence_without_static.java
@@ -0,0 +1,20 @@
+package Super_150.Recursion;
+public class count_subsequence_without_static {
+    public static void main(String[] args) {
+        String str="abc";
+        String ans="";
+//    subSeq(str, ans);
+        System.out.println(subSeq(str,ans));
+    }
+    public static int subSeq(String str, String ans) {
+        if (str.length() == 0) {
+            return 1;
+        }
+        char ch = str.charAt(0);
+        int a=subSeq(str.substring(1), ans);
+        int b=subSeq(str.substring(1), ans + ch);
+        return a+b;
+    }
+}
+
+
