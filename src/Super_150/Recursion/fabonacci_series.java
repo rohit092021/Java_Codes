ed13af0d6a941f201f43c5bd893900909d5235b2
Rohit <rohit.gla_cs21@gla.ac.in>
2023-01-16 20:06:47 +0530

    java code

--- src/Super_150/Recursion/fabonacci_series.java
+++ src/Super_150/Recursion/fabonacci_series.java
@@ -1,18 +1,8 @@
 package Super_150.Recursion;
-
+import java.util.*;
 public class fabonacci_series {
     public static void main(String[] args) {
-//        int a=0;
-//        int b=1;
-        int n=10;
-        System.out.println(febo(n));
-    }
-    public static int febo(int n){
-        if(n== 0 || n==1){
-            return n;
-        }
-        int f1=febo(n-1);
-        int f2=febo(n-2);
-        return f1+f2;
+
+
     }
 }
