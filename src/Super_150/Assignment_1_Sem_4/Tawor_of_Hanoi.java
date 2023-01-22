1d3a13fbfff11c901fa2a360e4f637e9a79e126b
Rohit <rohit.gla_cs21@gla.ac.in>
2023-01-22 18:44:08 +0530

    java code

---
+++ src/Super_150/Assignment_1_Sem_4/Tawor_of_Hanoi.java
@@ -0,0 +1,20 @@
+package Super_150.Assignment_1_Sem_4;
+
+import java.util.Scanner;
+
+public class Tawor_of_Hanoi {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        TowerOfHanoi(n,"s","d","h");
+
+    }
+    public static void TowerOfHanoi(int n, String s, String d, String h){
+        if(n<=0){
+            return;
+        }
+        TowerOfHanoi(n-1,s,h,d);
+        System.out.println("Moving ring " + n + " from " + s +" to "+ d);
+        TowerOfHanoi(n-1,h,d,s);
+    }
+}
