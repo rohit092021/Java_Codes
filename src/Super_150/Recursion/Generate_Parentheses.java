f6d2818601ca534309f5dfb04e7fe8b95342902d
Rohit <rohit.gla_cs21@gla.ac.in>
2023-01-22 18:43:47 +0530

    java code

--- src/Super_150/Recursion/Generate_Parentheses.java
+++ src/Super_150/Recursion/Generate_Parentheses.java
@@ -1,27 +1,32 @@
-//package Super_150.Recursion;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class Generate_Parentheses {
-//    public static void main(String[] args) {
-//        int n=3;
-//        List<String> ll=new ArrayList<>();
-//        Parenthesis(n,"",0,0);
-//        System.out.println(ll);
-//    }
-//    public static void Parenthesis(int n, String ans, int open, int close){
-//        if(open==n && close == n){
-////            System.out.println(ans+" ");
-//            ll.add(ans);
-//            return;
-//        }
-//        if(open < n){
-//            Parenthesis(n, ans+"(", open+1, close);
-//        }
-//        if(close<open){
-//            Parenthesis(n,ans + ")",open, close+1);
-//        }
-//
-//    }
-//}
+package Super_150.Recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generate_Parentheses {
+
+        public static void main(String[] args) {
+            // TODO Auto-generated method stub
+            int n = 2;
+            List<String> ll = new ArrayList<>();
+            Parentheses(n, "", 0, 0,ll);
+            System.out.println(ll);
+
+        }
+
+        public static void Parentheses(int n, String ans, int open, int close,List<String> ll) {
+            if (open ==n&& close==n) {
+                //System.out.print(ans + " ");
+                ll.add(ans);
+                return;
+            }
+            if (open < n) {
+                Parentheses(n, ans + "(", open + 1, close,ll);
+            }
+            if (close < open) {
+                Parentheses(n, ans + ")", open, close + 1,ll);
+            }
+
+        }
+    }
+
