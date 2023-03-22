380992ae017dac8bce0cc020ab41e252c3f77fa5
Rohit <rohit.gla_cs21@gla.ac.in>
2023-03-22 15:48:06 +0530

    java code

---
+++ src/Super_150/Stack/Last_element.java
@@ -0,0 +1,24 @@
+package Super_150.Stack;
+
+import java.util.Stack;
+
+public class Last_element {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(1);
+        st.push(21);
+        st.push(19);
+        st.push(11);
+        st.push(18);
+        System.out.println(LastValue(st));
+    }
+    public static int LastValue(Stack<Integer> st){
+        if(st.size()==1){
+    return st.peek();
+        }
+        int item=st.pop();
+        int x=LastValue(st);
+        st.push(item);
+        return x;
+    }
+}
