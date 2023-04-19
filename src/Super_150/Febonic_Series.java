f7a8b15f7cdc907552e14c8c660efdfe27ddc5bd
Rohit <rohit.gla_cs21@gla.ac.in>
2023-04-19 15:37:39 +0530

    java code

--- src/Super_150/Febonic_Series.java
+++ src/Super_150/Febonic_Series.java
@@ -6,7 +6,7 @@ public class Febonic_Series {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int num=sc.nextInt();
-        int a=0;
+        int a=0; // for a=0 index will start from 1 and for a = 1 index will start from 0
         int b=1;
 
         for(int i=1;i<=num;i++)
@@ -14,6 +14,7 @@ public class Febonic_Series {
            int c=a+b;
             a=b;
             b=c;
+//            System.out.println(a);
         }
         System.out.println(a);
     }
