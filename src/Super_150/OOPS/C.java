3e24f227ab56be6b03725e171c0e071feb196faa
Rohit <rohit.gla_cs21@gla.ac.in>
2023-04-19 15:37:14 +0530

    java code

---
+++ src/Super_150/OOPS/C.java
@@ -0,0 +1,12 @@
+package Super_150.OOPS;
+
+public class C {
+    int d=2;
+    int d2=20;
+    public void fun(){
+        System.out.println("Fun in C");
+    }
+    public void fun2(){
+        System.out.println("Fun2 in C");
+    }
+}
