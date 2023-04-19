4c7bbb7b024b99fbd2eb0796cef59c20e44cffb8
Rohit <rohit.gla_cs21@gla.ac.in>
2023-04-19 15:37:27 +0530

    java code

---
+++ src/Super_150/Queue/Dynamic_Queue.java
@@ -0,0 +1,19 @@
+//package Super_150.Queue;
+//
+//public class Dynamic_Queue {
+//    public static void main(String[] args) throws Exception {
+//        Dynamic_Queue dq= new Dynamic_Queue();
+//        dq.Enqueue(1);
+//        dq.Display(2);
+//        Reverse(dq);
+//        dq.Display();
+//    }
+//    public static void reverse(Dynamic_Queue dq) throws Exception{
+//        if(dq.isEmpty()){
+//            return;
+//        }
+//        int x=dq.Dequeue();
+//        Reverse(dq);
+//        dq.Enqueue(x);
+//    }
+//}
