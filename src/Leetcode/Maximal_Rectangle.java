23bb474a0f73bc69f6a466c644e0c674e26b09db
Rohit <rohit.gla_cs21@gla.ac.in>
2023-04-26 16:34:42 +0530

    java code

---
+++ src/Leetcode/Maximal_Rectangle.java
@@ -0,0 +1,11 @@
+//package Leetcode;
+//
+//public class Maximal_Rectangle {
+//    public static void main(String[] args) {
+//        int matrix[][] ={{1,0,1,0,0},{1,0,1,1,1},{1,1,1,1,1},{1,0,0,1,0}};
+//        int [] arr = new int[matrix[].length;
+//    }
+//    public static int Area(int matrix[][]){
+//
+//    }
+//}
